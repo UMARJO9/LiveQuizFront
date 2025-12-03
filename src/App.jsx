40b59b71ca3c5c4e3bf56c65df54fb37238de5bc
UMARJO9 <umarnurmadov12@gmail.com>
2025-12-03 22:27:43 +0500

    fix color

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import AuthPage from './pages/AuthPage'
 import MainPage from './pages/MainPage'
+import {useState} from "react";
 
 function App() {
-  const [isAuthed, setIsAuthed] = useState(false)
+  const [isAuthed, setIsAuthed] = useState(() => {
+    return Boolean(localStorage.getItem('token'))
+  })
 
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    setIsAuthed(Boolean(token))
-  }, [])
-
-  return isAuthed ? <MainPage /> : <AuthPage onAuth={() => setIsAuthed(true)} />
+  return isAuthed ? (
+      <MainPage />
+  ) : (
+      <AuthPage onAuth={() => setIsAuthed(true)} />
+  )
 }
 
 export default App
